0cb1ede618d0b6621e8b14df842c1f771e04faf9
Julien Freyermuth <julien.chipster@gmail.com>
2017-12-19 15:36:41 +0100

    A mistake

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/julien/Programmation/django/github/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- exo/short_url/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from .models import URL
-
-# Créer une url avec un racourci
-# Si url_long existe déjà
-# Si url_short existe déjà
-
-# Faire plusieurs insert dans la db et vérifier que j'ai bien la liste
