77b1012830c23765e2121c8edaf33d4eefbc705a
Julien Freyermuth <julien.chipster@gmail.com>
2017-12-20 11:01:54 +0100

    Adding a test

--- exo/short_url/tests.py
+++ exo/short_url/tests.py
@@ -7,3 +7,14 @@ from .models import URL
 # Si url_short existe déjà
 
 # Faire plusieurs insert dans la db et vérifier que j'ai bien la liste
+
+
+# Écrire un test qui crée un enregistrement
+class URLTests(TestCase):
+    def create_an_entry(self):
+        """
+        Create an entry in the db and test if it exists
+        """
+        new_url = URL(url_long="www.google.fr")
+        url_in_db = URL.objects.get(url_long="www.google.fr")
+        self.assertEqual(new_url, url_in_db)
